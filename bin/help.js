bd18b906d25f85d076e0c6b42c430fff26873903
Connor <cdy@email.sc.edu>
2018-09-30 07:56:40 -0400

    make 'help' display known commands

--- bin/help.js
+++ bin/help.js
@@ -1,9 +1,22 @@
+var knownCommands = [
+	"help", "shutdown", "reboot",
+	"source"
+];
+
 function loadedFunc(params)
 {
 	txt += "\n";
 
 	if (params[1] == "me") txt += "You're on your own."; 
-	else txt += "There is no help.";
+	else
+	{
+		txt += "The following commands are available:\n";
+		knownCommands.forEach((command, index) =>
+		{
+			txt += command;
+			if (index < knownCommands.length - 1) txt += ", ";
+		});
+	}
 
 	typeWriter();
 }
